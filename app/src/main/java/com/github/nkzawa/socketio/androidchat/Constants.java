d2c70684ed3139e906bb98d27edb41a160dbbd78
daimaqiao <daimaqiao@126.com>
2019-07-18 15:00:06 +0800

    add project: socket.io-client-java

--- app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
@@ -1,5 +1,8 @@
 package com.github.nkzawa.socketio.androidchat;
 
 public class Constants {
-    public static final String CHAT_SERVER_URL = "https://socket-io-chat.now.sh/";
+    //public static final String CHAT_SERVER_URL = "https://socket-io-chat.now.sh/";
+    public static final String CHAT_SERVER_URL = "http://192.168.0.200:3000/";
+    //public static final String CHAT_SERVER_URL = "http://wifi.local.daimaqiao.net:3000/";
+    //public static final String CHAT_SERVER_URL = "https://synapse.local.daimaqiao.net/";
 }
